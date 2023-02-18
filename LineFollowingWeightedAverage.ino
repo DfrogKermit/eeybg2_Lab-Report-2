34180e58afd5cc977249753131628a31e6e96885
DfrogKermit <118816427+DfrogKermit@users.noreply.github.com>
2023-02-18 17:06:24 +0000

    Update LineFollowingWeightedAverage.ino

--- LineFollowingWeightedAverage.ino
+++ LineFollowingWeightedAverage.ino
@@ -1,54 +1,62 @@
 #include <WiFi.h>
 #include <Wire.h>
-#define green 35 
-#define pink 26
-#define purple 32
-#define yellow 34
-#define blue 25
-#define white 33
+#define I2C_SLAVE_ADDR 0x04 
 
-//3 pairs of sensors for each side
-const int leftSensorsPins[] = {26, 32, 25};
-const int rightSensorsPins[] = {35, 34, 33};
-const int leftMotorPin = 14;
-const int rightMotorPin = 5;
+// THESE WILL BE THE 2 USED SENSORS 
+#define SENSOR_LEFT 25 
+#define SENSOR_RIGHT 33
+
+#define SENSOR_LEFT_WEIGHT 37
+#define SENSOR_RIGHT_WEIGHT 20
+//THRESHOLD OF THE RANGE OF THE SENSORS
+#define SENSOR_THRESHOLD 3500
+
 
 void setup() {
-  Serial.begin(115200);
-  Wire.begin(21, 22);
-  for (int i = 0; i < 3; i++) {
-    pinMode(leftSensorsPins[i], INPUT);
-    pinMode(rightSensorsPins[i], INPUT);
-  }
-  pinMode(leftMotorPin, OUTPUT);
-  pinMode(rightMotorPin, OUTPUT);
+  Serial.begin(9600);
+  Wire.begin();
+  pinMode(SENSOR_LEFT,INPUT);
+  pinMode(SENSOR_RIGHT,INPUT);
 }
-
-void loop() {
-  int weightedAverage = 0;
-  for (int i = 0; i < 3; i++) {
-    int leftSensorsValue = digitalRead(leftSensorsPins[i]);
-    int rightSensorsValue = digitalRead(rightSensorsPins[i]);
-    // Use the weighted average formula to calculate the direction of the line
-    weightedAverage += (leftSensorsValue * -1 + rightSensorsValue * 1) / 2;
-  }
-  weightedAverage /= 3;
-  int motorCommand = 0;
-
-  if (weightedAverage == 0) {
-    // Line is in the center, move forward
-    Serial.println("Move Forward");
-    motorCommand = 1;
-  } else if (weightedAverage > 0) {
-    // Line is on the right side, steer left
-    Serial.println("Steer Left");
-    motorCommand = 2;
-  } else if (weightedAverage < 0) {
-    // Line is on the left side, steer right
-    Serial.println("Steer Right");
-    motorCommand = 3;
-  }
-  Wire.beginTransmission(8);
-  Wire.write(motorCommand);
+/*This function is in charge of thE movemnet of the car, including data being 
+sent to the Arduino to adjust thE motor speeds and the servo angle. */
+void moveCar(int servoAngleint, leftMotor_speed, int rightMotor_speed) {
+  Wire.beginTransmission(I2C_SLAVE_ADDR); 
+  Wire.write((byte)((leftMotor_speed & 0x0000FF00) >> 8));
+  Wire.write((byte)(leftMotor_speed & 0x000000FF));
+  Wire.write((byte)((rightMotor_speed & 0x0000FF00) >> 8));
+  Wire.write((byte)(rightMotor_speed & 0x000000FF)); 
   Wire.endTransmission();
+  delay(200);
+}
+//Here the use of thw weighted average formula is seen.
+void loop() {
+  int sensorLeftValue = analogRead(SENSOR_LEFT);
+  int sensorRightValue = analogRead(SENSOR_RIGHT);
+
+  double error = 0;
+  if (sensorLeftValue < SENSOR_THRESHOLD && sensorRightValue < SENSOR_THRESHOLD) {
+    error = 0;
+  } else if (sensorLeftValue < SENSOR_THRESHOLD) {
+    error = -1;
+  } else if (sensorRightValue < SENSOR_THRESHOLD) {
+    error = 1;
+  } else {
+    error = ((double)sensorLeftValue / (double)sensorRightValue) - 1;
+  }
+
+  int leftMotorSpeed = 0;
+  int rightMotorSpeed = 0;
+
+  if (error == 0) {
+    leftMotorSpeed = 120;
+    rightMotorSpeed = 100;
+  } else {
+    leftMotorSpeed = 120 + (error * 10);
+    rightMotorSpeed = 100 - (error * 10);
+  }
+
+  moveCar(leftMotorSpeed, rightMotorSpeed);
+
+  delay(50);
 }
